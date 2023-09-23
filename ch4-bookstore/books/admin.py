4e86e187b48286085d440cfe9e590989e9bd8066
Amir Hossein Rajabi <108143052+amirh8r1@users.noreply.github.com>
2023-09-23 14:35:18 +0330

    ch12-ReviewsApp

--- ch4-bookstore/books/admin.py
+++ ch4-bookstore/books/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Review
 
 # Register your models here.
 
+class ReviewInline(admin.TabularInline):
+    model = Review
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewInline,
+    ]
     list_display = (
         "title",
         "author",
